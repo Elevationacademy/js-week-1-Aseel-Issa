dd6192f736f2362be3ded3c8a36025ecbcb48ac9
aseel <aseelissa91@hotmail.com>
2020-10-22 14:17:50 +0300

    Commiting the six ecercises of thursday's week

---
+++ main.js
@@ -0,0 +1,58 @@
+
+// Start of exercise one
+const maxCapacity = 14
+let surfTime = true
+let bestStudent
+// const purposeInLife
+
+console.log(maxCapacity)
+console.log(surfTime)
+console.log(bestStudent)
+console.log("------")
+// console.log(purposeInLife)
+// End of exercise two
+
+// Start of exercise two
+let cookies = "Cookies"
+let result = cookies + " is the best thing ever"
+console.log(result)
+console.log("------")
+// End of exercise two
+
+// Start of exercise three
+let password = "myPassword"
+let confirmPassword = "mypassword"
+if(password != confirmPassword){
+    console.log("The two passwords are different, please try again!")
+}
+else{
+    console.log("The two passwords are identical");
+}
+console.log("------")
+// End of exercise three
+
+// Start of excersice four
+// finding the result of 423 X 12 statement
+console.log("The result of 423 multiplied by 12 is: "+(423*12))
+// finding the result of 802 / 2 statement
+console.log("The result of 802 divided by 2 is: "+(802/2))
+// finding the result of the sum of 5 and 6, multiplied by 3 statement
+console.log("The result of the sum of 5 and 6, multiplied by 3 is: "+((5+6)*3))
+console.log("------")
+// End of exercise four
+
+// start of exercise five
+// false
+// true
+// true
+// false
+// true
+// true
+// false
+// true
+// false
+// End of ercise five
+
+// Start of exercise six
+// a,b equals 0, and c equals 2
+// End of ercise six
